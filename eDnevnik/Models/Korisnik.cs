2d23823aa00f9b5921a5aea4ef6aa05725b2dac8
Aldin Velić <avelic5@etf.unsa.ba>
2025-05-14 23:23:44 +0100

    Dodan dijagram sekvence

--- eDnevnik/Models/Korisnik.cs
+++ eDnevnik/Models/Korisnik.cs
@@ -1,6 +1,14 @@
-﻿namespace eDnevnik.Models
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace eDnevnik.Models
 {
-    public class Korisnik
+    public class Korisnik : IdentityUser
     {
+        public string Ime { get;set;}
+
+        public string Prezime { get; set; }
+
+
     }
 }
